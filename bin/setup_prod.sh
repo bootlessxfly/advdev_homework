d378024f3e30c1d4292bfeca03caf8ae9a95d3f9
chwhite <chwhite@redhat.com>
2019-11-18 10:13:49 -0600

    - Changed setup files

--- bin/setup_prod.sh
+++ bin/setup_prod.sh
@@ -20,11 +20,12 @@ oc expose dc tasks-blue --port 8080 -n ${GUID}-tasks-prod
 oc create configmap tasks-blue-config --from-literal="application-users.properties=Placeholder" --from-literal="application-roles.properties=Placeholder" -n ${GUID}-tasks-prod
 oc set volume dc/tasks-blue --add --name=jboss-config --mount-path=/opt/eap/standalone/configuration/application-users.properties --sub-path=application-users.properties --configmap-name=tasks-blue-config -n ${GUID}-tasks-prod
 oc set volume dc/tasks-blue --add --name=jboss-config1 --mount-path=/opt/eap/standalone/configuration/application-roles.properties --sub-path=application-roles.properties --configmap-name=tasks-blue-config -n ${GUID}-tasks-prod
-oc set probe dc/tasks-blue --readiness --get-url=http://:8080/ --initial-delay-seconds=30 --timeout-seconds=60 -n ${GUID}-tasks-prod
-oc set probe dc/tasks-blue --liveness --get-url=http://:8080/ --initial-delay-seconds=30 --timeout-seconds=60 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-blue --readiness --get-url=http://:8080/ --initial-delay-seconds=20 --timeout-seconds=20 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-blue --liveness --get-url=http://:8080/ --initial-delay-seconds=20 --timeout-seconds=20 -n ${GUID}-tasks-prod
 # Setting 'wrong' VERSION. This will need to be updated in the pipeline
 oc set env dc/tasks-blue VERSION='0.0 (tsks-blue)' -n ${GUID}-tasks-prod
 
+oc set resources dc tasks-blue --limits=memory=4Gi,cpu=4 --requests=memory=3Gi,cpu=2 -n ${GUID}-tasks-prod
 
 # Create Green Application
 oc new-app ${GUID}-tasks-dev/tasks:0.0 --name=tasks-green --allow-missing-imagestream-tags=true -n ${GUID}-tasks-prod
@@ -33,10 +34,14 @@ oc expose dc tasks-green --port 8080 -n ${GUID}-tasks-prod
 oc create configmap tasks-green-config --from-literal="application-users.properties=Placeholder" --from-literal="application-roles.properties=Placeholder" -n ${GUID}-tasks-prod
 oc set volume dc/tasks-green --add --name=jboss-config --mount-path=/opt/eap/standalone/configuration/application-users.properties --sub-path=application-users.properties --configmap-name=tasks-green-config -n ${GUID}-tasks-prod
 oc set volume dc/tasks-green --add --name=jboss-config1 --mount-path=/opt/eap/standalone/configuration/application-roles.properties --sub-path=application-roles.properties --configmap-name=tasks-green-config -n ${GUID}-tasks-prod
-oc set probe dc/tasks-green --readiness --get-url=http://:8080/ --initial-delay-seconds=30 --timeout-seconds=60 -n ${GUID}-tasks-prod
-oc set probe dc/tasks-green --liveness --get-url=http://:8080/ --initial-delay-seconds=30 --timeout-seconds=60 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-green --readiness --get-url=http://:8080/ --initial-delay-seconds=20 --timeout-seconds=20 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-green --liveness --get-url=http://:8080/ --initial-delay-seconds=20 --timeout-seconds=20 -n ${GUID}-tasks-prod
 # Setting 'wrong' VERSION. This will need to be updated in the pipeline
 oc set env dc/tasks-green VERSION='0.0 (tsks-green)' -n ${GUID}-tasks-prod
+
+# Tune dc resources
+oc set resources dc tasks-blue --limits=memory=2Gi,cpu=1 --requests=memory=2Gi,cpu=1 -n ${GUID}-tasks-prod
+oc set resources dc tasks-green --limits=memory=2Gi,cpu=1 --requests=memory=2Gi,cpu=1 -n ${GUID}-tasks-prod
 
 # Expose Blue service as route to make green application active
 oc expose svc/tasks-green --name tasks -n ${GUID}-tasks-prod
